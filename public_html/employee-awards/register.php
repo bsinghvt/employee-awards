8d8b10b5dcb0332f6b28f3f8599ca33563bbfbde
Baljot Singh <baljotsingh1986@yahoo.com>
2016-07-01 20:23:50 -0700

    Update register.php

--- public_html/employee-awards/register.php
+++ public_html/employee-awards/register.php
@@ -2,7 +2,7 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-require_once("../../website-files/includes/initialize.php");
+require_once(__DIR__.'/website-files/includes/initialize.php');
 if(isset($_POST['submit'])) {
     $username = trim($_POST['username']);
 	 $password = trim($_POST['password']);
