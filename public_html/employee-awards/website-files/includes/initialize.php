2cd188d432737691027accb5ce27c66649744e0f
Baljot Singh <baljotsingh1986@yahoo.com>
2016-07-01 20:01:45 -0700

    Update initialize.php

--- public_html/employee-awards/website-files/includes/initialize.php
+++ public_html/employee-awards/website-files/includes/initialize.php
@@ -8,9 +8,9 @@ defined("LIB_PATH") ? null : define("LIB_PATH", SITE_ROOT.DS.'includes');
 require_once(LIB_PATH.DS."config.php");
 require_once(LIB_PATH.DS."functions.php");
 require_once(LIB_PATH.DS."session.php");
-//require_once(LIB_PATH.DS."database.php");
-//require_once(LIB_PATH.DS."database-object.php");
-//require_once(LIB_PATH.DS."user.php");
-//require_once(LIB_PATH.DS."admin.php");
-//require_once(LIB_PATH.DS."awards.php");
+require_once(LIB_PATH.DS."database.php");
+require_once(LIB_PATH.DS."database-object.php");
+require_once(LIB_PATH.DS."user.php");
+require_once(LIB_PATH.DS."admin.php");
+require_once(LIB_PATH.DS."awards.php");
 ?>
