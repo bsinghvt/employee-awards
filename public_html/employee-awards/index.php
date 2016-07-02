316ee323c41c64ca27c3404cb5ac6ace7adb7b61
Baljot Singh <baljotsingh1986@yahoo.com>
2016-07-01 20:41:18 -0700

    Update index.php

--- public_html/employee-awards/index.php
+++ public_html/employee-awards/index.php
@@ -1,7 +1,7 @@
 <?php
 require_once(__DIR__.'/website-files/initialize.php');
-//if(!$session->is_logged_in()) {redirect_to("login.php");}
-//get_template("header.php");
+if(!$session->is_logged_in()) {redirect_to("login.php");}
+get_template("header.php");
 ?>
-<?php //get_template("footer.php"); 
+<?php get_template("footer.php"); 
 echo "<h1>Hello World</h1>"; ?>
