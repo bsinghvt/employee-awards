adf5e7a389e99e371f0bd13fc8e3543d86560f87
Baljot Singh <Baljot Singh>
2016-07-01 18:46:59 -0700

    Updating Initialize php

--- website-files/includes/initialize.php
+++ website-files/includes/initialize.php
@@ -1,7 +1,7 @@
 <?php
 //Database constants
 defined("DS") ? null : define("DS", DIRECTORY_SEPARATOR);
-defined("SITE_ROOT") ? null : define("SITE_ROOT", DS.'var'.DS.'www'.DS.'gallery-files');
+defined("SITE_ROOT") ? null : define("SITE_ROOT", DS.'website-files');
 defined("LIB_PATH") ? null : define("LIB_PATH", SITE_ROOT.DS.'includes');
 require_once(LIB_PATH.DS."config.php");
 require_once(LIB_PATH.DS."functions.php");
