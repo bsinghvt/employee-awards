b46ec080f77cef8d048b3fdfd07ef6fda702cc17
Baljot Singh <Baljot Singh>
2016-07-02 14:11:33 -0700

    Updated awards.php to reflect current database

--- public_html/employee-awards/website-files/includes/awards.php
+++ public_html/employee-awards/website-files/includes/awards.php
@@ -14,13 +14,13 @@ class Award extends DatabaseObject {
     public $r_middle_name;
    
     protected static $param_type = 'ssss';
-    protected static $insert_query = 'INSERT INTO Award (recepient_email, password, r_first_name, r_last_name, r_middle_name, award_type, granted, public) VALUES(?, ?, ?, ?, ?, ?, ?, ?)';
+    protected static $insert_query = 'INSERT INTO Award (recepient_email, r_first_name, r_last_name, r_middle_name, award_type, granted, public, uid) VALUES(?, ?, ?, ?, ?, ?, ?, ?)';
     protected static $select_query_all = 'SELECT * from Award';
  
     function __construct(){
     }
     protected function insert_params(){
-        return array(self::$param_type, $this->recepient_email, $this->password, $this->r_first_name, $this->r_last_name, $this->r_middle_name, $this->award_type, $this->granted, $this->public);
+        return array(self::$param_type, $this->recepient_email, $this->r_first_name, $this->r_last_name, $this->r_middle_name, $this->award_type, $this->granted, $this->public, $this->uid);
     }
     public function findAll(){
         return parent::any_select_query(self::$select_query_all);
