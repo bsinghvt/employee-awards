d6341fd8c4d907a17124e6f7ab91397a535ee90b
Harrings <sean.harrington2@gmail.com>
2016-07-02 12:48:01 -0700

    added user registration

---
+++ public_html/employee-awards/createuser.php
@@ -0,0 +1,110 @@
+<?php
+ob_start(); //from stack overflow
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+require_once(__DIR__.'/website-files/initialize.php');
+$error=0;
+if(isset($_POST['user_email'])) {
+	include 'pass.php';
+	$mysqli = new mysqli("oniddb.cws.oregonstate.edu", "harrings-db", $pass, "harrings-db");
+    if ($mysqli->connect_errno) {
+		echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+	}
+	$accounts=array();
+	if (!$stmt = $mysqli->query("SELECT user_email FROM User_Account")) {
+			echo "Query Failed!: (" . $mysqli->errno . ") ". $mysqli->error;
+		}
+	while($row = mysqli_fetch_array($stmt))
+	{
+		if (((!in_array($row['user_email'], $accounts)))&&($row['user_email']!=null))
+		{
+			array_push($accounts,$row['user_email']);
+		}	
+	}
+	if (($_POST["user_email"]==null))
+	{
+		echo "Error to add an account it must have an email click <a href=\"register.php\">here</a> to return to account creation screen";
+	}
+	else if (($_POST["password"]==null))
+	{
+		echo "Error to add an account it must have a password click <a href=\"register.php\">here</a> to return to account creation screen";
+	}
+	else if (($_POST["signature"]==null))
+	{
+		echo "Error to add an account it must have a signature click <a href=\"register.php\">here</a> to return to account creation screen";
+	}
+	else if (($_POST["first_name"]==null))
+	{
+		echo "Error to add an account it must have a first name click <a href=\"register.php\">here</a> to return to account creation screen";
+	}
+	else if (($_POST["last_name"]==null))
+	{
+		echo "Error to add an account it must have a last name click <a href=\"register.php\">here</a> to return to account creation screen";
+	}
+	else if (($_POST["job_title"]==null))
+	{
+		echo "Error to add an account it must have a job title click <a href=\"register.php\">here</a> to return to account creation screen";
+	}
+	else if (in_array($_POST['user_email'], $accounts))
+	{
+		echo "Could not add account as there is already another user with that user_email click <a href=\"register.php\">here</a> to return to account creation screen";
+	}
+	else
+	{
+		$user_email=$_POST["user_email"];
+		$password=$_POST["password"];
+		$signature=$_POST["signature"];
+		$first_name=$_POST["first_name"];
+		$last_name=$_POST["last_name"];
+		$job_title=$_POST["job_title"];
+		$creation=date('Y-m-d H:i:s');
+		$middle_name=$_POST["middle_name"];
+		if (($_POST["middle_name"]==null))
+		{
+			$middle_name= "none";
+		}
+		if (!($stmt = $mysqli->prepare("INSERT INTO User_Account(user_email, password, creation , signature, first_name, middle_name, last_name, job_title) VALUES (?,?,?,?,?,?,?,?)"))) {
+			 echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
+			 $error=1;
+		}
+		if (!$stmt->bind_param("ssssssss", $user_email, $password, $creation, $signature, $first_name, $middle_name, $last_name, $job_title)) {
+			echo "Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
+			$error=1;
+		}
+		if (!$stmt->execute()) {
+			//echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+			$error=1;
+		}
+		$stmt->close();
+		if ($error==0)
+		{
+			$message= "registered successfully";
+			/*
+			session_start();
+			$_SESSION["user_email"]=$_POST["user_email"];
+			$_SESSION["units"]=$units;
+			$_SESSION["teacher"]=$teacher;
+			header("Location: switch.php", true);
+			*/
+		}
+		else
+		{
+			echo "Error there is already an account with that user name click <a href=\"index.php\">here</a> to return to login page";
+		}
+	}
+}
+else
+{
+	$message = "Unable to register user as registration form was not completed";
+}	
+get_template("header.php");
+echo output_message($message); 
+?>
+
+
+
+
+
+
+<?php get_template($template = "footer.php"); ?>
--- public_html/employee-awards/register.php
+++ public_html/employee-awards/register.php
@@ -2,45 +2,24 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-require_once(__DIR__.'/website-files/initialize.php');
-if(isset($_POST['submit'])) {
-    $username = trim($_POST['username']);
-	 $password = trim($_POST['password']);
-	 $firstname = trim($_POST['firstname']);
-	 $lastname = trim($_POST['lastname']);
-    if(!isset($username) || !isset($password) || !isset($firstname) || !isset($lastname) || empty($username) || empty($password) || empty($firstname) || empty($lastname)) {
-		 $message = "Registeration Failed. Please enter all the fields correctly.";
-		 unset($_POST['submit']);
-	 } else {
-		 $newUser = new User();
-		 $newUser->username = $username;
-		 $newUser->password = $password;
-		 $newUser->first_name = $firstname;
-		 $newUser->last_name = $lastname;
-		 if($newUser->createUser()){
-			 $session->message = "Registration is successfull";
-			 log_actions('Registration', "{$username} registered.");
-			 unset($_POST['submit']);
-			 redirect_to("login.php");		 
-		 } else {
-			 $message = "Registration Failed. ".$newUser->error;
-			 unset($_POST['submit']);
-		 }
-    }
-} 
+require_once(__DIR__.'/website-files/initialize.php'); 
 get_template("header.php");
 echo output_message($message); 
 ?>
 
 <div class="form-group">
-    <form method="post" action="register.php"> 
+    <form method="post" action="createuser.php"> 
         <fieldset>
 				<legend>Registration</legend>
-				<p>First Name: <input type="text" name="firstname" class="form-control" maxlength="30"/></p>
-            <p>Last Name: <input type="text" name="lastname" class="form-control" maxlength="30"/></p>
-            <p>Username: <input type="text" name="username" class="form-control" maxlength="30"/></p>
-            <p>Password: <input type="password" name="password" class="form-control" maxlength="30"/></p>
-            <p><input type="submit" name="submit" class="btn btn-default" value="Register"/></p>
+				<p>User email (will be username): <input type="email" name="user_email" required /></p>
+		<p>Password: <input type="password" name="password" required /></p>
+		<p>First name: <input type="text" name="first_name" required /></p>
+		<p>Middle name (optional): <input type="text" name="middle_name" /></p>
+		<p>Last name: <input type="text" name="last_name" required /></p>
+		<p>Job Title: <input type="text" name="job_title" required /></p>
+		<p>Signature:  <input type="file" name="signature" accept="image/*">
+		<br><br>
+		<input type="submit" value="Submit">
         </fieldset>
 	 </form>
 </div>
