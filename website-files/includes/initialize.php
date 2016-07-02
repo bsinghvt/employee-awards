2baee500a6d5d5ec0f74584e383c8e1a300e6989
Baljot Singh <baljotsingh1986@yahoo.com>
2016-07-01 18:55:11 -0700

    Update initialize.php

--- website-files/includes/initialize.php
+++ website-files/includes/initialize.php
@@ -1,11 +1,9 @@
 <?php
 //Database constants
 defined("DS") ? null : define("DS", DIRECTORY_SEPARATOR);
-<<<<<<< HEAD
+
 defined("SITE_ROOT") ? null : define("SITE_ROOT", DS.'website-files');
-=======
-defined("SITE_ROOT") ? null : define("SITE_ROOT", DS.'var'.DS.'www'.DS.'website-files');
->>>>>>> fa41eb31ce0ee3d27a79acb086464f9882a78c48
+
 defined("LIB_PATH") ? null : define("LIB_PATH", SITE_ROOT.DS.'includes');
 require_once(LIB_PATH.DS."config.php");
 require_once(LIB_PATH.DS."functions.php");
