21ac86978a4b8fcbc57578b61522360f2dbd8f7e
Baljot Singh <Baljot Singh>
2016-07-02 14:09:10 -0700

    Updated awards.php to reflect current database

--- public_html/employee-awards/website-files/includes/awards.php
+++ public_html/employee-awards/website-files/includes/awards.php
@@ -1,66 +1,29 @@
 <?php
-require_once(LIB_PATH.DS."database.php");
-
-//Comment class extends DatabaseObject
-class Comment extends DatabaseObject {
-	protected static $table_name = "comments";
-   public $id,
-    		 $uid,
-			 $pid,
-			 $createdon,
-			 $body;
+require_once(LIB_PATH.DS.'database.php');
+class Award extends DatabaseObject {
+    protected static $table_name = 'Award';
+    public $r_first_name;
+    public $recepient_email;
+    public $granted;
+    public $award_type;
+    public $r_last_name;
+    public $uid;
+	public $adid;
+    public $error;
+    public $public;
+    public $r_middle_name;
+   
+    protected static $param_type = 'ssss';
+    protected static $insert_query = 'INSERT INTO Award (recepient_email, password, r_first_name, r_last_name, r_middle_name, award_type, granted, public) VALUES(?, ?, ?, ?, ?, ?, ?, ?)';
+    protected static $select_query_all = 'SELECT * from Award';
  
-	//Function to upload comment details into database
-   public function addComment() {
-        $this->createdon = strftime("%Y-%m-%d %H:%M:%S", time());
-        global $database;
-        $sql = "INSERT INTO ".self::$table_name." (uid, pid, createdon, body) VALUES(?, ?, ?, ?)";
-        $stmt =& $database->query($sql);
-        if(!($stmt->bind_param("iiss", $uid, $pid, $createdon, $body))){
-            echo "Bind failed: "  . $stmt->errno . " " . $stmt->error;
-            return false;
-        }
-        $uid = $this->uid;
-        $pid = $this->pid;
-        $createdon = $this->createdon;
-        $body = $this->body;
-        if(!($stmt->execute())) {
-        echo "Execute failed: "  . $stmt->errno . " " . $stmt->error;
-            return false;
-        }
-        $this->id = $stmt->insert_id;
-        return true;
-   }
-
-	 //Function to update a comment
-    public function updateComment() {
-        global $database;
-        $sql = "UPDATE ".self::$table_name." SET body=?, createdon=? WHERE id=?";
-        $stmt =& $database->query($sql);
-        if(!($stmt->bind_param("ssi", $body, $createdon, $ID))){
-            echo "Bind failed: "  . $stmt->errno . " " . $stmt->error;
-            exit;
-        }
-        $body = $this->body;
-        $createdon = strftime("%Y-%m-%d %H:%M:%S", time());
-        $ID = $this->id;
-        if(!($stmt->execute())) {
-            echo "Execute failed: "  . $stmt->errno . " " . $stmt->error;
-            exit;
-        }
-	 }
-
-	 public function &findCommentsOnPhoto(){
-		 if(!isset($this->pid) || $this->pid == 0){
-		 	return false;
-		 }
-		 $sql = "SELECT * FROM ".static::$table_name." WHERE pid = ".$this->pid;
-		 return Parent::findAll($sql);
-	 }
-  
-	 public function &findAllAdmin(){
-         $sql = "SELECT * FROM ".static::$table_name;
-         return Parent::findAll($sql);
-   }
+    function __construct(){
+    }
+    protected function insert_params(){
+        return array(self::$param_type, $this->recepient_email, $this->password, $this->r_first_name, $this->r_last_name, $this->r_middle_name, $this->award_type, $this->granted, $this->public);
+    }
+    public function findAll(){
+        return parent::any_select_query(self::$select_query_all);
+    }
 }
 ?>
