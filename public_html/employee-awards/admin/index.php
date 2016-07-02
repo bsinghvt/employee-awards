b210bebb307f93d8152b618e754a6168f6d0a7ab
Baljot Singh <Baljot Singh>
2016-07-02 16:42:07 -0700

    Update admin/index.php

--- public_html/employee-awards/admin/index.php
+++ public_html/employee-awards/admin/index.php
@@ -5,7 +5,7 @@ error_reporting(E_ALL);
 require_once(__DIR__.'/../website-files/initialize.php');
 $session->check_adm_login();
 if(!$session->is_admin_logged_in()) {redirect_to("login.php");}
-get_template("header.php");
+get_template("admin-header.php");
 ?>
 <?php get_template("footer.php"); 
  ?>
