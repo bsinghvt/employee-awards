afcf8b54125d21a4986a0d70067c9326d58debc2
Baljot Singh <Baljot Singh>
2016-07-02 17:12:38 -0700

    Created admin/normal-users.php

---
+++ public_html/employee-awards/admin/normal-users.php
@@ -0,0 +1,46 @@
+<?php
+/*ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);*/
+require_once(__DIR__.'/../website-files/initialize.php');
+?>
+<?php 
+$session->check_adm_login();
+if(!$session->is_admin_logged_in()){
+    redirect_to('login.php');
+} 
+get_template("admin-header.php");
+
+?>
+<div id="normal-users">
+                <p><h5>All Normal Users</h5></p>
+            
+                <table id="dispcourse" class="table table-striped">
+                    <tr>
+                        <th>First Name</th>
+                        <th>Middle Name</th>
+                        <th>Last Name</th>
+                        <th>Job Title</th>
+                        <th>Last Updated Time</th>
+                    </tr>
+                
+                <!-- PHP-->
+<?php
+    $data = [];
+    $users = new User();
+    if(!$data = $users->findAll()){
+        echo '<p style="color:red;"><b>Error in database. The users cannot be displayed. Please try again</b></p';
+    }
+    foreach($data as $info): ?>
+ <tr id="<?php echo $info->uid ?>">
+     <td><?php echo $info->first_name; ?></td>
+     <td><?php echo $info->middle_name; ?></td>
+     <td><?php echo $info->last_name; ?></td>
+     <td><?php echo $info->job_title; ?></td>
+	 <td><?php echo $info->creation; ?></td>
+</tr>
+
+<?php endforeach; ?>
+                </table>
+</div>
+<?php get_template($template = "footer.php"); ?>
