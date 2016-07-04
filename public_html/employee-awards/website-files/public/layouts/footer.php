629aabcbe7723577b08075d63a9ea50d4a1c00be
Baljot Singh <Baljot Singh>
2016-07-04 12:37:42 -0700

    update footer

--- public_html/employee-awards/website-files/public/layouts/footer.php
+++ public_html/employee-awards/website-files/public/layouts/footer.php
@@ -3,6 +3,7 @@
             <p >Copyright&copy;&nbsp;<?php echo date("Y", time());?>&nbsp;Green Arrow Consulting</p>
         </footer>
 		<script src="<?php if(isset($script)){echo $script;} ?>" type="text/javascript"></script>
+		<script src="https://cdn.datatables.net/1.10.12/js/jquery.dataTables.min.js"></script>
     </body>
 </html>
 <?php 
