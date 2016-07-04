8db977e3c4dbe9fcc160d81d47f5822cf456e20d
Baljot Singh <Baljot Singh>
2016-07-04 12:38:22 -0700

    update style and script

--- public_html/employee-awards/public/javascripts/script.js
+++ public_html/employee-awards/public/javascripts/script.js
@@ -1,3 +1,4 @@
+//Function to delete a user
 function deleteNormalUser(key, URL){
     var row = document.getElementById(key);
     $.ajax({url: URL,
@@ -9,3 +10,34 @@ function deleteNormalUser(key, URL){
                 }
     }});
 }
+
+//Function to filter usesrs https://datatables.net/examples/api/
+$(document).ready(function() {
+    // Setup - add a text input to each footer cell
+    $('#displaytable tfoot th').each( function () {
+        var title = $(this).text();
+        if(title.toUpperCase() === 'ACTION'){
+            $(this).html( '<input type="hidden" />' );
+        }
+        else{
+            $(this).html( '<input type="text" placeholder="Search '+title+'" />' );
+        }
+        
+    } );
+ 
+    // DataTable
+    var table = $('#displaytable').DataTable();
+ 
+    // Apply the search
+    table.columns().every( function () {
+        var that = this;
+ 
+        $( 'input', this.footer() ).on( 'keyup change', function () {
+            if ( that.search() !== this.value ) {
+                that
+                    .search( this.value )
+                    .draw();
+            }
+        } );
+    } );
+} );
