d5217797f136e0ef407ec0a3bf93dc67ebbc1dd6
Baljot Singh <Baljot Singh>
2016-07-04 10:42:11 -0700

    Update addnewuserform.php

--- public_html/employee-awards/website-files/public/layouts/addnewuserform.php
+++ public_html/employee-awards/website-files/public/layouts/addnewuserform.php
@@ -1,5 +1,5 @@
 <div class="form-group required">
-    <form method="post" action="<?php echo $action; ?>"> 
+    <form enctype="multipart/form-data" method="post" action="<?php echo $action; ?>"> 
         <fieldset>
 				<legend><?php echo $legend; ?></legend>
 		<label class="control-label" for="useremail">User email (will be username): </label><input id="useremail" class="form-control" type="email" name="user_email" required />
@@ -8,7 +8,8 @@
 		<label for="usermiddlename">Middle name (optional): </label><input id="usermiddlename" class="form-control" type="text" name="middle_name" />
 		<label class="control-label" for="userlastname">Last name: </label><input id="userlastname" class="form-control" type="text" name="last_name" required />
 		<label class="control-label" for="userjobtitle">Job Title: </label><input id="userjobtitle" class="form-control" type="text" name="job_title" required />
-		<label class="control-label" for="usersign">Signature: </label><input id="usersign" class="form-control" type="file" name="signature" accept="image/*">
+		<label for="maxsize"><input id="maxsize" class="form-control" type="hidden" name="MAX_FILE_SIZE" value="204800" />
+		<label class="control-label" for="usersign">Signature (Max : 200KB): </label><input id="usersign" class="form-control" type="file" name="signature" accept="image/*">
 		<br>
 		<input class="btn btn-default" type="submit" value="Submit">
 		<p style="color:red;"> <b>(*) denotes required fields.</b></p>
