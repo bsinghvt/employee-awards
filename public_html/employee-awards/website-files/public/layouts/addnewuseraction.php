af6ce38e2c1c73aeff379d5608275bc5a374a339
Baljot Singh <Baljot Singh>
2016-07-04 10:01:33 -0700

    Updated addnewuseraction.php to insert sign image to database

--- public_html/employee-awards/website-files/public/layouts/addnewuseraction.php
+++ public_html/employee-awards/website-files/public/layouts/addnewuseraction.php
@@ -5,14 +5,23 @@ if(!empty($_POST)){
 		
 		if(trim($_POST["user_email"]) != "" && trim($_POST["password"]) != ""
 		&& trim($_POST["first_name"]) != "" && trim($_POST["last_name"]) != "" && trim($_POST["job_title"]) != "") {
-			if (isset($_FILES['signature']) && $_FILES['signature']['size'] > 0){ 
-				$data = file_get_contents($_FILES['signature']['tmp_name']);
+			if (isset($_FILES['signature']) && $_FILES['signature']['size'] > 0){
+				if($_FILES['signature']['size'] > 204800){
+					$GLOBALS['msg'] = '<p style="color:red;"> <b>Error: Image of your signature cannot be greater than 200KB.</b></p>';
+					return;
+				}
+				$allowedTypes = array(IMAGETYPE_PNG, IMAGETYPE_JPEG, IMAGETYPE_GIF);
+				$detectedType = exif_imagetype($_FILES['signature']['tmp_name']);
+				if(!in_array($detectedType, $allowedTypes)){
+					$GLOBALS['msg'] = '<p style="color:red;"> <b>Error: Only JPG, JPEG, PNG & GIF files are allowed for signature.</b></p>';
+				}
 				$image_check = getimagesize($_FILES['signature']['tmp_name']);
 				if($image_check==false)
 				{
-					$GLOBALS['msg'] = '<p style="color:red;"> <b>Please upload a valid image of your signature.</b></p>';
+					$GLOBALS['msg'] = '<p style="color:red;"> <b>Error: Please upload a valid image of your signature.</b></p>';
 					return;
 				}
+				$data = file_get_contents($_FILES['signature']['tmp_name']);
 				$user = new User();
 				$user->user_email=$_POST["user_email"];
 				$user->password=$_POST["password"];
