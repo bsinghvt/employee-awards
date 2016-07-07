8f07dcc40a98bb1ed49180d39969e16f81bb3d0c
Baljot Singh <Baljot Singh>
2016-07-06 21:48:19 -0700

    Updates footer

--- public_html/employee-awards/website-files/public/layouts/footer.php
+++ public_html/employee-awards/website-files/public/layouts/footer.php
@@ -4,6 +4,7 @@
         </footer>
 		<script src="<?php if(isset($script)){echo $script;} ?>" type="text/javascript"></script>
 		<script src="<?php if(isset($export_table)){echo $export_table;} ?>" type="text/javascript"></script>
+		<script src="https://code.jquery.com/jquery-1.12.3.js"></script>
 		<script src="https://cdn.datatables.net/1.10.12/js/jquery.dataTables.min.js"></script>
     </body>
 </html>
