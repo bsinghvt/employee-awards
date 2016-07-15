a2eea1da1daa348d9d02577b0c281decc24c4078
Harrings <sean.harrington2@gmail.com>
2016-07-15 14:01:39 -0700

    added public award viewing

--- public_html/employee-awards/index.php
+++ public_html/employee-awards/index.php
@@ -12,7 +12,46 @@ if (!isset($_SESSION["user_email"]))
 get_template("header.php");
 get_template("nav2.php");
 ?>
-
-<p> "You are at the core" </p>
+<h2>Most Recent Public Awards</h2>
+<table border="1">
+<thead> 
+<tr>
+    <th>Recepeient first name</th> 
+    <th>Recepeient middle name</th> 
+	<th>Recepeient last name</th> 
+    <th>Recepeient email</th> 
+    <th>Date Granted</th> 
+    <th>Awarder first name</th> 
+	    <th>Awarder middle name</th> 
+	<th>Awarder last name</th>
+</tr> 
+</thead>
+<tbody>
+<?php
+include 'pass.php';
+	$mysqli = new mysqli("oniddb.cws.oregonstate.edu", "harrings-db", $pass, "harrings-db");
+    if ($mysqli->connect_errno) {
+		echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+	}
+if (!$stmt = $mysqli->query("SELECT Award.award_type, Award.recepient_email, Award.r_first_name, Award.r_middle_name, Award.r_last_name, Award.granted, Award.Public, User_Account.first_name,User_Account.middle_name, User_Account.last_name FROM Award INNER JOIN User_Account on Award.uid=User_Account.uid WHERE Award.Public=1 ORDER BY Award.adid DESC LIMIT 5"))
+	{
+		echo "Query Failed!: (" . $mysqli->errno . ") ". $mysqli->error;
+	}
+while($row = mysqli_fetch_array($stmt))	
+{
+	echo "<tr>" ;
+	echo "<td>" . $row['r_first_name'] . "</td>";
+	echo "<td>" . $row['r_middle_name'] . "</td>";
+	echo "<td>" . $row['r_last_name'] . "</td>";
+	echo "<td>" . $row['recepient_email'] . "</td>";
+	echo "<td>" . $row['granted'] . "</td>";
+	echo "<td>" . $row['first_name'] . "</td>";
+		echo "<td>" . $row['middle_name'] . "</td>";
+			echo "<td>" . $row['last_name'] . "</td>";
+	echo "<tr>" ;
+}
+?>
+</tbody>
+</table>
 <?php get_template("footer.php"); 
  ?>
