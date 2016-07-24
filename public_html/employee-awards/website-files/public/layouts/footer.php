767c4c0efb922ea19757a53dbe08ed943e4353dd
Baljot Singh <Baljot Singh>
2016-07-24 14:22:33 -0700

    Changed footer with latest jquery

--- public_html/employee-awards/website-files/public/layouts/footer.php
+++ public_html/employee-awards/website-files/public/layouts/footer.php
@@ -2,8 +2,11 @@
         <footer>
             <p >Copyright&copy;&nbsp;<?php echo date("Y", time());?>&nbsp;Green Arrow Consulting</p>
         </footer>
-		<script src="https://code.jquery.com/jquery-1.12.3.js"></script>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 		<script src="https://cdn.datatables.net/1.10.12/js/jquery.dataTables.min.js"></script>
+		<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+		 <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+		 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.6.1/js/bootstrap-datepicker.min.js"></script>
 		<script src="<?php if(isset($export_table)){echo $export_table;} ?>" type="text/javascript"></script>
 		<script src="<?php if(isset($script)){echo $script;} ?>" type="text/javascript"></script>
     </body>
