36d4411ef3379dc88e2a9270a816e0e503418dea
Harrings <sean.harrington2@gmail.com>
2016-08-11 12:11:36 -0700

    fixed delete confirm

--- public_html/employee-awards/deleteaconfirm.php
+++ public_html/employee-awards/deleteaconfirm.php
@@ -4,7 +4,7 @@ ob_start(); //from stack overflow
 include 'pass.php';
 error_reporting(E_ALL);
 ini_set('display_errors','On');
-session_start();
+//session_start();
 if (!isset($_SESSION["user_email"]))
 {
     header("Location: login.php", true);
@@ -13,6 +13,6 @@ get_template("header.php");
 get_template("nav2.php");
 ?>
 <p>Are you sure you wish to delete your account: Note all awards you have given will be deleted from database as well </p>
-<p><a href="deleteuser.php">Click here </a> to delete or <a href="edituserinfo.php">Return to account editing</a>
+<p><a href="deleteuser.php">Click here </a> to delete or <a href="edituser.php">Return to account editing</a>
 <?php get_template("footer.php"); 
  ?>
